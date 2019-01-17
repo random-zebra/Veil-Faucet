0fb4cea7b368029edb0f19843f46586b6a539dea
random-zebra <random.zebra@protonmail.com>
2019-01-17 12:01:06 +0100

    add favicon

--- templates/default.template.php
+++ templates/default.template.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-
+   <link rel="icon" href="favicon.ico" type="image/x-icon"/>
 	<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 	<meta name="author" content="Cryptocurrency faucet script" />
 
@@ -18,6 +18,8 @@
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
     <![endif]-->
+
+    <meta property="og:image" content="https://veil-project.com/icons/logo.png" />
 
     {{HEAD}}
 
